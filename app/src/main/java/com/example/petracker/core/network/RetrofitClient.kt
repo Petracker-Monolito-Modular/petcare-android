bc808143ac2925a46209ab636b77c1021637e573
Jackpot <brunoruizn@gmail.com>
2025-10-25 11:33:20 -0300

    fix: moshi

--- app/src/main/java/com/example/petracker/core/network/RetrofitClient.kt
+++ app/src/main/java/com/example/petracker/core/network/RetrofitClient.kt
@@ -1,6 +1,9 @@
+// core/network/RetrofitClient.kt
 package com.example.petracker.core.network
 
 import com.example.petracker.core.storage.TokenStore
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -9,6 +12,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitClient {
+    // Emulador: 10.0.2.2 mapea a localhost de tu PC
     private const val BASE_URL = "http://10.0.2.2:8000/"
 
     fun create(tokenStore: TokenStore): Retrofit {
@@ -22,17 +26,23 @@ object RetrofitClient {
             } else original
             chain.proceed(req)
         }
+
         val logging = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
+
         val client = OkHttpClient.Builder()
             .addInterceptor(auth)
             .addInterceptor(logging)
             .build()
 
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi)) // <- usa el moshi de arriba
             .client(client)
             .build()
     }
